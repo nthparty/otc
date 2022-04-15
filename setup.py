1269edafb788aba0130f17aa780e91a25cf01439
Andrei Lapets <a@lapets.io>
2022-04-15 15:55:04 -0400

    Update dependency version locks and package version.

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.rst", "r") as fh:
     long_description = fh.read().replace(".. include:: toc.rst\n\n", "")
 
 name = "otc"
-version = "2.0.0"
+version = "3.0.0"
 
 setup(
     name=name,
     version=version,
     packages=["otc",],
     install_requires=[
-        "bcl~=2.0",
-        "oblivious~=4.0"
+        "bcl~=2.1",
+        "oblivious~=5.0"
     ],
     license="MIT",
     url="https://github.com/nthparty/otc",
